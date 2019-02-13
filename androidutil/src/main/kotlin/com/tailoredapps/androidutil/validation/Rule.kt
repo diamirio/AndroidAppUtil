322d264405e0e504c0e663f9b8d61ae50b32c556
flosch <florian.schuster@tailored-apps.com>
2019-02-13 15:03:20 +0100

    Fixed NotNull Rule

--- androidutil/src/main/kotlin/com/tailoredapps/androidutil/validation/Rule.kt
+++ androidutil/src/main/kotlin/com/tailoredapps/androidutil/validation/Rule.kt
@@ -30,31 +30,27 @@ interface Rule<T : Any> {
     fun validate(input: T?): Boolean
 }
 
-class NotNullRule(@StringRes override val errorMessage: Int) : Rule<Any> {
-    override fun validate(input: Any?) = input != null
+class NotNullRule<T : Any>(@StringRes override val errorMessage: Int) : Rule<T> {
+    override fun validate(input: T?) = input != null
 }
 
 class NotEmptyRule(@StringRes override val errorMessage: Int) : Rule<String> {
     override fun validate(input: String?) = input != null && input.isNotEmpty()
 }
 
-class EqualRule(private val other: () -> String?, @StringRes override val errorMessage: Int) :
-    Rule<String> {
+class EqualRule(private val other: () -> String?, @StringRes override val errorMessage: Int) : Rule<String> {
     override fun validate(input: String?) = input == other.invoke()
 }
 
-open class RegexRule(private val pattern: String, @StringRes override val errorMessage: Int) :
-    Rule<String> {
+open class RegexRule(private val pattern: String, @StringRes override val errorMessage: Int) : Rule<String> {
     override fun validate(input: String?) = input != null && pattern.toRegex().matches(input)
 }
 
-class MinLengthRule(private val length: Int, @StringRes override val errorMessage: Int) :
-    Rule<String> {
+class MinLengthRule(private val length: Int, @StringRes override val errorMessage: Int) : Rule<String> {
     override fun validate(input: String?): Boolean = input != null && input.length >= length
 }
 
-class MaxLengthRule(private val length: Int, @StringRes override val errorMessage: Int) :
-    Rule<String> {
+class MaxLengthRule(private val length: Int, @StringRes override val errorMessage: Int) : Rule<String> {
     override fun validate(input: String?): Boolean = input != null && input.length <= length
 }
 
@@ -78,14 +74,13 @@ class DateAfterTodayRule(@StringRes override val errorMessage: Int) : Rule<Local
     override fun validate(input: LocalDate?): Boolean = input != null && input.isAfter(LocalDate.now())
 }
 
-class EmailRule(@StringRes errorMessage: Int) :
-    RegexRule(
-        "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
-                "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
-                "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
-                "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
-                "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
-                "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
-                "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
-        errorMessage
-    )
+class EmailRule(@StringRes errorMessage: Int) : RegexRule(
+    "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
+            "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+            "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
+            "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
+            "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+            "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+            "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+    errorMessage
+)
